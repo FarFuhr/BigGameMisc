feb613561ab854b273cd868640292ab2056bf63f
David Zolotov <zdraste111@gmail.com>
2025-10-28 23:17:27 +0300

    Script for Isaac's SteamCloud disabling

---
+++ isaac_disable_steam_cloud.ps1
@@ -0,0 +1,13 @@
+$documents = [environment]::getfolderpath("mydocuments")
+$optionsFile = Join-Path -Path $documents -ChildPath '$My Games/Binding of Isaac Repentance/options.ini'
+if (-Not ([System.IO.File]::Exists($path))) {
+	write "options.ini not found"
+	pause
+	exit -1
+}
+else {
+	(gc $optionsFile) -replace 'SteamCloud=1', 'SteamCloud=0' | Out-File -encoding ASCII $optionsFile
+	write "Steam Cloud disabled"
+	pause
+	exit 0
+}
