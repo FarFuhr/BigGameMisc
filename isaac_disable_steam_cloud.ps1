6a884e69be4f7615b7e489607821b59eafcd6ccf
David Zolotov <zdraste111@gmail.com>
2025-10-29 03:54:23 +0300

    Fix typos in script

--- isaac_disable_steam_cloud.ps1
+++ isaac_disable_steam_cloud.ps1
@@ -1,11 +1,10 @@
 $documents = [environment]::getfolderpath("mydocuments")
-$optionsFile = Join-Path -Path $documents -ChildPath '$My Games/Binding of Isaac Repentance/options.ini'
-if (-Not ([System.IO.File]::Exists($path))) {
-	write "options.ini not found"
+$optionsFile = Join-Path -Path $documents -ChildPath 'My Games\Binding of Isaac Repentance+\options.ini'
+if (-Not ([System.IO.File]::Exists($optionsFile))) {
+	write "Could not find file: $optionsFile"
 	pause
 	exit -1
-}
-else {
+} else {
 	(gc $optionsFile) -replace 'SteamCloud=1', 'SteamCloud=0' | Out-File -encoding ASCII $optionsFile
 	write "Steam Cloud disabled"
 	pause
